7a119b9cfe3ff96f03f0406bc4a7064eb2764526
Bharat Sharma <40849224+bharatbrijwasi@users.noreply.github.com>
2020-03-27 11:50:43 +0530

    first update
    i try to commit

--- src/problem1/main/MyMain.java
+++ src/problem1/main/MyMain.java
@@ -7,5 +7,13 @@
 package problem1.main;
 // executable class
 
-public class MyMain {
+public class MyMain 
+{
+	public static void main(String[] args)
+	{
+		int a = 10;
+		int b = 20;
+		int add = a+b;
+		System.out.println(add);
+	}
 }
